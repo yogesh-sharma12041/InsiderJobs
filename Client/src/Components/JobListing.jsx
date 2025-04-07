0a20615eac172743a9d2c50e997db92a92907b0c
Yogesh Sharma <yogesh.sharma12041@gmail.com>
2025-04-07 21:23:34 +0530

    Added improvements

--- Client/src/Components/JobListing.jsx
+++ Client/src/Components/JobListing.jsx
@@ -13,7 +13,7 @@ const JobListing = () => {
   const [selectedCategories, setSelectedCategories] = useState([]);
   const [selectedLocations, setSelectedLocations] = useState([]);
 
-  const [filteredJobs, setFilteredJobs] = useState(jobs);
+  const [filteredJobs, setFilteredJobs] = useState([]);
 
   const handleCategoryChange = (category) => {
     setSelectedCategories(
